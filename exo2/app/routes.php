6d377f3f6a0e066954a188a7b5adefb50fa48622
Alex <alexandre.cahay@hetic.net>
2017-06-29 22:24:37 +0200

    exo 2 - add actors

--- exo2/app/routes.php
+++ exo2/app/routes.php
@@ -17,6 +17,7 @@ $app->get('/', function () use ($app) {
 $app->get('/movie/{id}', function ($id) use ($app) {
     $sql = "SELECT * FROM movie WHERE movie_id = ?";
     $movie = $app['db']->fetchAssoc($sql, array((int) $id));
+
     $director_id = "";
     foreach ($movie as $key => $value) {
         if ($key == 'movie_director') {
@@ -24,7 +25,16 @@ $app->get('/movie/{id}', function ($id) use ($app) {
         }
     }
     $director = $app['db']->fetchAssoc("SELECT person_name, person_firstName FROM person WHERE person_id = ?", array((int) $director_id));
-    return $app['twig']->render('show.html.twig', array('movie' => $movie, 'director' => $director));
+
+    $actors_id = $app['db']->fetchAll("SELECT actor_person FROM actor WHERE actor_movie = ?", array((int) $id));
+    $actors = [];
+    foreach ($actors_id as $actor_id_array) {
+        foreach ($actor_id_array as $actor_person => $actor_person_id) {
+            $actor = $app['db']->fetchAssoc("SELECT person_name, person_firstName, person_id FROM person WHERE person_id = ?", array((int) $actor_person_id));
+            array_push($actors, $actor);
+        }
+    }
+    return $app['twig']->render('show.html.twig', array('movie' => $movie, 'director' => $director, 'actors' => $actors));
 })->bind('show');
 
 /**
@@ -47,6 +57,12 @@ $app->get('/add', function (Request $request) use ($app) {
     $director = $request->get('director');
     move_uploaded_file($poster, "/web/uploads" );
     $app['db']->insert('movie', array('movie_title' => $title, 'movie_synopsis' => $synopsis, 'movie_date' => $date, 'movie_poster' =>$poster, 'movie_director' => $director));
+
+    $movie_id = $app['db']->lastInsertId();
+    $actors_array = $request->get('actors');
+    foreach ($actors_array as $actor) {
+        $app['db']->insert('actor', array('actor_person' => $actor, 'actor_movie' => $movie_id));
+    }
     return $app->redirect($app['url_generator']->generate('home'));
 })->bind('add');
 
@@ -93,3 +109,21 @@ $app->get('/add-director', function (Request $request) use ($app) {
     $app['db']->insert('person', array('person_name' => $name, 'person_firstName' => $firstName, 'person_birthDate' => $born));
     return $app->redirect($app['url_generator']->generate('home'));
 })->bind('add-director');
+
+/**
+ * SHOW PERSON
+ */
+$app->get('/person/{id}', function ($id) use ($app) {
+    $sql = "SELECT * FROM person WHERE person_id = ?";
+    $person = $app['db']->fetchAssoc($sql, array((int) $id));
+
+    $movies_data = $app['db']->fetchAll("SELECT actor_movie FROM actor WHERE actor_person = ?", array((int) $id));
+    $movies = [];
+    foreach ($movies_data as $actor_movie => $actor_movie_id) {
+        foreach ($actor_movie_id as $key => $value) {
+            $movie = $app['db']->fetchAssoc("SELECT * FROM movie WHERE movie_id = ?", array((int) $value));
+            array_push($movies, $movie);
+        }
+    }
+    return $app['twig']->render('actor.html.twig', array('person' => $person, 'movies' => $movies));
+})->bind('actor');
--- exo2/db/database.sql
+++ exo2/db/database.sql
@@ -3,6 +3,7 @@ use IMDB;
 
 grant all privileges on IMDB.* to 'alex'@'localhost' identified by 'alex';
 
+drop table if exists actor;
 drop table if exists movie;
 drop table if exists person;
 
@@ -23,10 +24,22 @@ create table movie (
   constraint fk_movie_director foreign key(movie_director) references person(person_id)
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
+create table actor (
+  actor_id integer not null primary key auto_increment,
+  actor_person integer not null,
+  actor_movie integer not null,
+  constraint fk_actor_person foreign key(actor_person) references person(person_id),
+  constraint fk_actor_movie foreign key(actor_movie) references movie(movie_id)
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
 insert into person values
   (1, 'Scott', 'Ridley', '1937-11-30');
 insert into person values
   (2, 'Cameron', 'James', '1954-08-16');
+insert into person values
+  (3, 'Crowe', 'Russell', '1964-04-07');
+insert into person values
+  (4, 'Phoenix', 'Joaquin', '1974-10-28');
 
 insert into movie values
   (1, 'Gladiator', 'When a Roman general is betrayed and his family murdered by an emperor''s corrupt son, he comes to Rome as a gladiator to seek revenge.', '2000-06-01', 'uploads/glad.jpg', 1);
@@ -35,3 +48,7 @@ insert into movie values
 insert into movie values
   (3, 'Lorem ipsum in french', "J’en dis autax devores, et que rien ne nous empêche de faire ce qui peut nous donner le plus de plaisir, nous pouvons nous livrer entièrement à la volupté et chasser toute sorte de douleur ; mais, dans les temps destinés aux devoirs de la société ou à la nécessité des affaires, souvent il faut faire divorce avec la volupté, et ne se point refuser à la peine. La règle que suit en cela un homme sage, c’est de renoncer à de légères voluptés pour en avoir de plus grandes, et de savoir supporter des douleurs légères pour en éviter de plus fâcheuses.", '2017-06-01', '', 1);
 
+insert into actor values
+  (1, 3, 1);
+insert into actor values
+  (2, 4, 1)
